aee72e3cb23323d0a79a16c965721048532fb9a5
sekian <anassbenali@gmail.com>
2021-12-05 17:53:08 +0100

    added basic UI

--- script.js
+++ script.js
@@ -1,48 +1,114 @@
+let playerScore = 0;
+let computerScore = 0;
+const message = "Choose Rock, Paper or Scissors!";
+const buttons = document.querySelectorAll('button')
+
 function computerPlay() {
     choices = ["rock", "paper", "scissors"];
     const randomIndex = Math.floor(Math.random() * choices.length);
     return choices[randomIndex];
 }
 
+function disableButtons() {
+    buttons.forEach(elem => {
+        elem.disabled = true
+    })
+}
+
+function enableButtons() {
+    buttons.forEach(elem => {
+        elem.disabled = false
+    })
+}
+
 function playRound(playerSelection, computerSelection) {
-    playerSelection.toLowerCase();
+    playerSelection = playerSelection.toLowerCase();
     if (playerSelection == computerSelection)
         return 'Draw!';
     switch (playerSelection) {
         case "rock":
             if (computerSelection == "paper")
-                return "You Lose! Paper beats Rock";
+                return "Player Lose! Paper beats Rock";
             else if (computerSelection == "scissors")
-                return "You Win! Rock beats Scissors";
+                return "Player Win! Rock beats Scissors";
             break;
         case "paper":
             if (computerSelection == "scissors")
-                return "You Lose! Scissors beats Paper";
+                return "Player Lose! Scissors beats Paper";
             else if (computerSelection == "rock")
-                return "You Win! Paper beats Rock";
+                return "Player Win! Paper beats Rock";
             break;
         case "scissors":
             if (computerSelection == "rock")
-                return "You Lose! Rock beats Scissors";
+                return "Player Lose! Rock beats Scissors";
             else if (computerSelection == "paper")
-                return "You Win! Scissors beats Paper";
+                return "Player Win! Scissors beats Paper";
             break;
     }
     return "Player did not select Rock, Paper or Scissors";
 }
 
-function game() {
-    playerWins = 0;
-    computerWins = 0;
-    const message = "Choose Rock, Paper or Scissors!";
-    for (let i = 0; i < 5; ++i) {
-        const playerSelection = window.prompt(message, "");;
-        const computerSelection = computerPlay();
-        result = playRound(playerSelection, computerSelection);
-        if (result.includes("Win")) ++playerWins;
-        else if (result.includes("Lose")) ++computerWins;
-        console.log(result);
+const rockButton = document.querySelector(".rock");
+const paperButton = document.querySelector(".paper");
+const scissorsButton = document.querySelector(".scissors");
+const divResult = document.querySelector(".result");
+
+buttons.forEach(button => {
+    button.addEventListener('click', function(){
+        result = playRound(button.textContent, computerPlay());
+        updateStats(result);
+    })
+})
+
+function updateStats(result) {
+    if (result.includes("Win")) ++playerScore;
+    else if (result.includes("Lose")) ++computerScore;
+
+    result = (result + "<br><br>Player score: " + playerScore +
+             "<br>Computer score: " + computerScore)
+    divResult.innerHTML = result;
+    console.log(`Player won ${playerScore} time(s)`);
+    console.log(`Computer won ${computerScore} time(s)`);
+
+    if (computerScore >= 5) {
+        result += '<br><br>The computer won the game!';
+        divResult.innerHTML = result;
+        disableButtons();
+        addResetButton();
     }
-    console.log(`Player won ${playerWins} time(s)`);
-    console.log(`Computer won ${computerWins} time(s)`);
+    if (playerScore >= 5) {
+        result += '<br><br>The player won the game!';
+        divResult.innerHTML = result;
+        disableButtons();
+        addResetButton();
+    }
+}
+
+function addResetButton() {
+    const btn = document.createElement("button");
+    btn.id = "resetButton";
+    btn.textContent = "Play again!";
+    btn.addEventListener('click', reset);
+    divResult.appendChild(document.createElement("br"));
+    divResult.appendChild(document.createElement("br"));
+    divResult.appendChild(btn);
+}
+
+function reset() {
+    computerScore = 0;
+    playerScore = 0;
+    divResult.textContent = "";
+    enableButtons();
+    updateStats(message)
+}
+
+function game() {
+    // for (let i = 0; i < 5; ++i) {
+    //     const playerSelection = window.prompt(message, "");;
+    //     const computerSelection = computerPlay();
+    //     result = playRound(playerSelection, computerSelection);
+    //     if (result.includes("Win")) ++playerScore;
+    //     else if (result.includes("Lose")) ++computerScore;
+    //     console.log(result);
+    // }
 }
