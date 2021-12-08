091f6d04ed0d8ea8054483718155b549b4e549d3
sekian <anassbenali@gmail.com>
2021-12-09 00:52:12 +0100

    layout

---
+++ script.js
@@ -0,0 +1,23 @@
+let currentOperation = null;
+let firstOperand = null;
+let secondOperand = null;
+
+function add(a, b) {
+    return a+b;
+}
+
+function substract(a, b) {
+    return a-b;
+}
+
+function multiply(a, b) {
+    return a*b;
+}
+
+function divide(a, b) {
+    return a/b;
+}
+
+function operate(operator, a, b) {
+    return operator(a, b);
+}
